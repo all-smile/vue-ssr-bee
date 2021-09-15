364720d00e642bc823c92f06ed237d352a00ec81
xiaojuntao <18143945157@163.com>
2021-09-15 13:39:20 +0800

    feat: 第一次提交，初始化项目

---
+++ build/webpack.client.js
@@ -0,0 +1,24 @@
+// build/webpack.client.js
+
+const webpack = require('webpack')
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
+const path = require('path')
+const resolve = dir => path.resolve(__dirname, dir)
+
+const base = require('./webpack.base')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = merge(base, {
+  entry: {
+    client: resolve('../src/entry-client.js')
+  },
+  plugins: isProd ? [
+    // new VueSSRClientPlugin(),
+  ] : [
+    new HtmlWebpackPlugin({
+      template: resolve('../public/index.html')
+    })
+  ]
+})
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-09-14 15:11:59
+ * @LastEditTime: 2021-09-14 20:06:30
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-ssr\pages\Home\index.vue
+-->
+<template>
+    <div class="ware">
+        test
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    props: {},
+    components: {},
+    data() {
+        return {}
+    },
+    computed: {},
+    watch: {},
+    created() { },
+    mounted() { },
+    methods: {},
+    updated() { },
+    beforeDestroy() { },
+}
+</script>
+
+<style lang='less' rel='stylesheet/less' scoped>
+// @import "./index.less";
+.ware {
+    background: #ddd;
+}
+</style>
